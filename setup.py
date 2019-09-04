b66d1b67361337df7dfd7418af24ff1053527223
France ROSE <frose@biocpc13.ens.fr>
2019-09-04 17:40:16 +0200

    setup.py pandas version fixed

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pySpacell',
-    version='0.1.4',
+    version='0.1.5',
     description='A Python package for single cell spatial image analysis',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -24,7 +24,7 @@ setup(
     packages=find_packages(exclude=['build', 'docs', 'templates', 'data']),
     include_package_data=True,
     install_requires=['matplotlib>=2.2.3', 'numpy', 'seaborn', 
-                      'PySAL>=2.0,<2.1', 'pandas<0.25','scipy', 
+                      'PySAL>=2.0,<2.1', 'pandas>=0.23,<0.24','scipy', 
                       'Pillow', 'opencv-python', 'scikit-image',
                       'scikit-learn',
                      ],
